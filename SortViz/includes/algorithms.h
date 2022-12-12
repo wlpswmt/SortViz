0dcba4612cabb9f5126359c7fa09ddfaf43fbc51
[wlpswmt] <[419317768@qq.com]>
2022-12-12 12:06:58 +0800

    first commit

---
+++ SortViz/includes/algorithms.h
@@ -0,0 +1,47 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <QThread>
+#include <QtWidgets>
+
+class Thread : public QThread
+{
+    Q_OBJECT
+public:
+    Thread(int, int, int, std::vector<double>, QObject*);
+
+signals:
+    void comparision(int, int);    //比较函数信号
+    void sortDone(int);            //排序信号
+    void arrayAccess(int);
+    void changeButtonStatus(int);
+
+protected:
+    void run() override;
+
+private:
+    std::vector<double> columnsHeight;
+    int sortDoneDelay;
+    int sortDelay;
+    int amount;
+    int sortWith;
+    int arrayAccessVariable;
+
+    void swap(int, int);
+    void isAccessToArray();
+    void Sorted();
+
+    //sorting algorithms declaration
+    void BubbleSort();
+    void RecursiveBubbleSort(int);
+    void CocktailSort();
+    void GnomeSort();
+
+    int QuickSortPartition(int, int);
+    void QuickSort(int, int);
+
+    void Heapify(int n, int i);
+    void HeapSort();
+};
+
+#endif // THREAD_H
